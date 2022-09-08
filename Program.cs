f723f25dbac459e82c6800f66d8e69288027c74e
je82443 <111613755+je82443@users.noreply.github.com>
2022-09-08 18:55:46 -0500

    ejercicio 5
    ejercicio 5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ejercicio5 
+{
+
+    }
+    
+    
+    public class program
+{
+    public static void Main()
+    {
+        double num2;
+        for (num2 = 0; num2 < 10; num2++) 
+        Console.WriteLine(Math.Pow(num2,2));
+
+    }
+}
